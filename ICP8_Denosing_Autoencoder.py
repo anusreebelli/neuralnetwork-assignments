0917a1a9320a2574c79f9dd9f5a0ce67aa461922
anusreebelli <156729380+anusreebelli@users.noreply.github.com>
2024-03-27 20:06:26 -0500

    Add files via upload

---
+++ ICP8_Denosing_Autoencoder.py
@@ -0,0 +1,34 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+# this model maps an input to its reconstruction
+autoencoder = Model(input_img, decoded)
+# this model maps an input to its encoded representation
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+from keras.datasets import fashion_mnist
+import numpy as np
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+#introducing noise
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
+
+autoencoder.fit(x_train_noisy, x_train,
+                epochs=10,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test_noisy, x_test_noisy))
